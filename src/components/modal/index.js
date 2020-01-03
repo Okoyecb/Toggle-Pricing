64a22db5fd58b234dbdcbaadd6cbbc240a837c85
Idris <idrisaboladeo@gmail.com>
2020-01-03 19:02:07 +0100

    adds modal for pricing component

---
+++ src/components/modal/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './modal.scss';
+
+const Modal = () => (
+    <div className='moda'>
+     
+    </div>
+)
+
+export default Modal;
