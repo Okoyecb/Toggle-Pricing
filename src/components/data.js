10533b6ad21413fd7aa2a119109f3c4de26c8707
Idris <idrisaboladeo@gmail.com>
2020-01-05 01:27:44 +0100

    adds static data to be used inside pricing component modal

---
+++ src/components/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+      Annualy: [
+        {
+          plan: 'Basic',
+          price: 199.99,
+          storage: '500GB',
+          users: 2,
+          GB_allocated: 3
+        },
+        {
+          plan: 'Professional',
+          price: 249.99,
+          storage: '1TB',
+          users: 5,
+          GB_allocated: 10
+        },
+        {
+          plan: 'Master',
+          price: 399.99,
+          storage: '2TB',
+          users: 10,
+          GB_allocated: 3
+        },
+        
+      ],
+      Monthly: [
+        {
+          plan: 'Basic',
+          price: 19.99,
+          storage: '500GB',
+          users: 2,
+          GB_allocated: 3
+        },
+        {
+          plan: 'Professional',
+          price: 24.99,
+          storage: '1TB',
+          users: 5,
+          GB_allocated: 10
+        },
+        {
+          plan: 'Master',
+          price: 39.99,
+          storage: '2TB',
+          users: 10,
+          GB_allocated: 3
+        },
+      ]
+    }
+];
+
+export default data
